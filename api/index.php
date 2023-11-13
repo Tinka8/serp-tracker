eb2b3ea4adac8ac094fd24c2bd5b39f4ba866d05
Tinka8 <lenmatka818@gmail.com>
2023-11-13 21:40:16 +0100

    get result from db

--- api/index.php
+++ api/index.php
@@ -1,18 +1,51 @@
 <?php
 
-// Údaje pro připojení k databázi
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "serp_tracker";
+$search_for_domain = $_GET["domain"] ?? null;
+$search_for_phrase = $_GET["phrase"] ?? null;
 
-// Vytvoření spojení
-$conn = new mysqli($servername, $username, $password, $dbname);
+include "../db.php";
 
-// Kontrola spojení
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$sql = "SELECT search_for_domain, search_for_phrase, min(position) as `position`, date(`current_time`) as `date` FROM serp_results";
+
+if ($search_for_domain) {
+  $sql .= " WHERE search_for_domain = '$search_for_domain'";
+} elseif ($search_for_phrase) {
+  $sql .= " WHERE search_for_phrase = '$search_for_phrase'";
 }
 
-// Dotaz na databázi
-$sql = "SELECT search_for_domain, search_for_phrase, position, `current_time` FROM serp_results";
+$sql .= " GROUP BY search_for_domain, search_for_phrase, date(`current_time`) ORDER BY date(`current_time`) ASC";
+
+$result = $conn->query($sql);
+
+$data = [];
+
+if ($result->num_rows > 0) {
+  // Procházení řádků
+  while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+  }
+} else {
+  echo json_encode(["error" => "No records found"]);
+  $conn->close();
+  exit();
+}
+
+// Nastavíme hlavičku odpovědi Content-type
+header("Content-Type: application/json");
+
+// Uzavření spojení
+$conn->close();
+
+// Get unique dates from data
+$labels = array_unique(array_column($data, "date"));
+
+// Get max fond position
+$max = max(array_column($data, "position"));
+
+// Group by search_for_domain and search_for_phrase
+$datasets = array_reduce($data, function ($carry, $item) {
+    $carry[$item["search_for_domain"]][$item["search_for_phrase"]][$item['date']] = $item;
+    return $carry;
+}, []);
+
+echo json_encode(compact('datasets', 'labels', 'max'));
