ab4397d972063946e78c8998ca822d6207cd9ed9
Tinka8 <lenmatka818@gmail.com>
2023-11-14 22:21:41 +0100

    ui

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+// /index.php
 
 $search_for_domain = $_GET["domain"] ?? null;
 $search_for_phrase = $_GET["phrase"] ?? null;
@@ -32,15 +33,31 @@ $phrases = json_decode(
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;500;700;800&display=swap" rel="stylesheet">
+
+        <style type="text/css">
+            .font-headline {
+                font-family: 'Kanit', sans-serif;
+            }
+        </style>
     </head>
     <body class="bg-zinc-200 py-12">
-        <div class="max-w-6xl mx-auto mb-2 bg-white rounded-xl px-6 shadow-sm overflow-hidden">
+        <div class="max-w-6xl mx-auto">
+            <h1 class="uppercase font-extrabold text-2xl pb-6">
+                <a href="/" class="text-black hover:text-zinc-800 tracking-tighter">
+                    SERP Tracker
+                </a>
+            </h1>
+        </div>
+        <div class="max-w-6xl mx-auto bg-white px-6 shadow-xl overflow-hidden z-20">
             <form class="grid grid-cols-4 divide-x divide-zinc-200">
                 <div class="py-4 px-6">
                     <label for="domain" class="text-sm font-bold mb-1 block">
                         Search for domain
                     </label>
-                    <select name="domain" id="domain" class="py-1 px-2 text-sm border border-zinc-200 rounded-sm">
+                    <select name="domain" id="domain" class="py-1 px-2 text-sm border border-t-zinc-300 bg-zinc-200 rounded-sm">
                         <option value="">All domains</option>
                         <?php foreach ($domains as $domain): ?>
                             <option value="<?php echo $domain[
@@ -58,7 +75,7 @@ $phrases = json_decode(
                     <label for="phrase" class="text-sm font-bold mb-1 block">
                         Search for phrase
                     </label>
-                    <select name="phrase" id="phrase" class="py-1 px-2 text-sm border border-zinc-200 rounded-sm">
+                    <select name="phrase" id="phrase" class="py-1 px-2 text-sm border border-t-zinc-300 bg-zinc-200 rounded-sm">
                         <option value="">All phrases</option>
                         <?php foreach ($phrases as $phrase): ?>
                             <option value="<?php echo $phrase[
@@ -74,21 +91,21 @@ $phrases = json_decode(
                 </div>
                 <div class="py-4">
                 </div>
-                <div class="justify-end flex items-center">
-                    <button type="submit" class="rounded shadow border border-zinc-200 bg-zinc-50 hover:bg-zinc-100 text-zinc-700 hover:text-zinc-800 px-3 py-1 hover:shadow-md">
+                <div class="justify-end flex items-center px-6">
+                    <button type="submit" class="rounded shadow-sm border border-lime-300 border-t-lime-100 bg-lime-400 hover:bg-lime-300 text-lime-900 hover:text-lime-950 px-3 py-1 hover:shadow-md">
                         Search
                     </button>
                 </div>
             </form>
         </div>
 
-        <div class="max-w-6xl mx-auto mb-12 bg-zinc-900 text-zinc-400 rounded-xl px-6 shadow-sm overflow-hidden">
-            <form class="grid grid-cols-4 divide-x divide-zinc-700" method="POST" action="/api/run">
+        <div class="max-w-6xl mx-auto mb-12 bg-zinc-800 text-zinc-400 rounded-b-xl px-6 shadow-sm overflow-hidden">
+            <form class="grid grid-cols-4 divide-x divide-transparent" method="POST" action="/api/run">
                 <div class="py-4 px-6">
                     <label for="search_domain" class="text-sm font-bold mb-1 block">
                         Search for domain
                     </label>
-                    <select name="domain" id="search_domain" class="py-1 px-2 text-sm border border-zinc-700 bg-zinc-950 rounded-sm" required>
+                    <select name="domain" id="search_domain" class="py-1 px-2 text-sm border border-transparent border-b-zinc-700 bg-zinc-900 rounded-sm" required>
                         <option value="">Select domain</option>
                         <?php foreach ($domains as $domain): ?>
                             <option value="<?php echo $domain[
@@ -106,11 +123,11 @@ $phrases = json_decode(
                     <label for="search_phrase" class="text-sm font-bold mb-1 block">
                         Search for phrase
                     </label>
-                    <input type="text" name="phrase" id="search_phrase" class="py-1 px-2 text-sm border border-zinc-700 bg-zinc-950 rounded-sm"/>
+                    <input type="text" name="phrase" id="search_phrase" class="py-1 px-2 text-sm border border-transparent border-b-zinc-700 bg-zinc-900 rounded-sm"/>
                 </div>
                 <div class="py-4 px-6">
                 </div>
-                <div class="justify-end flex items-center">
+                <div class="justify-end flex items-center px-6">
                     <button type="submit" class="rounded shadow border border-yellow-300 bg-yellow-400 hover:bg-yellow-500 text-yellow-900 hover:text-yellow-950 px-3 py-1 hover:shadow-md">
                         Get results
                     </button>
@@ -118,13 +135,13 @@ $phrases = json_decode(
             </form>
 
             <?php if (isset($_GET['message'])) : ?>
-                <div class="bg-zinc-950 border border-zinc-800 rounded-sm p-4 mb-4 text-xs text-white font-mono">
+                <div class="mx-6 bg-zinc-950 border border-zinc-800 rounded-sm p-4 mb-4 text-xs text-white font-mono">
                     <?php echo nl2br($_GET['message']); ?>
                 </div>
             <?php endif; ?>
         </div>
 
-        <div class="max-w-6xl mx-auto pb-2 bg-white rounded-xl shadow-lg overflow-hidden">
+        <div class="max-w-6xl mx-auto pb-2 bg-white rounded-xl shadow-lg overflow-hidden px-20 py-12">
             <div>
                 <canvas id="serpChart"></canvas>
             </div>
@@ -161,8 +178,15 @@ $phrases = json_decode(
                     scales: {
                         y: {
                             reverse: true,
-                            min: <?php echo $results['max']; ?>,
-                            max: 1,
+                            min: 1,
+                            max: <?php echo $results['max']; ?>,
+                            title: {
+                                display: true,
+                                text: 'Pozice'
+                            },
+                            ticks: {
+                                stepSize: 10
+                            }
                         }
                     }
                 }
