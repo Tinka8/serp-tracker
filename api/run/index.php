0b2a91cd2eac0cfeaa4ba114cb4689b60d6286c9
Tinka8 <lenmatka818@gmail.com>
2023-11-17 10:34:47 +0100

    run all pres button

--- api/run/index.php
+++ api/run/index.php
@@ -1,21 +1,34 @@
 <?php
 
-$domain = $_POST['domain'];
-$phrase = $_POST['phrase'];
-$number = $_POST['number'] ?? 100;
+$all = isset($_POST['all']) && $_POST['all'] === '1' ? true : false;
 
-$script = realpath(dirname(__FILE__) . "/../../main.py");
+$script = realpath(dirname(__FILE__) . '/../../main.py');
 
 // which python
 $python = '/opt/homebrew/opt/python@3/libexec/bin/python';
 
-$output = shell_exec(strtr(':python :script -d :domain -s ":phrase" -n :number', [
-    ':python' => $python,
-    ':script' => $script,
-    ':domain' => $domain,
-    ':phrase' => $phrase,
-    ':number' => $number,
-]));
+if (!$all) {
+    $domain = $_POST['domain'];
+    $phrase = $_POST['phrase'];
+    $number = $_POST['number'] ?? 100;
+
+    $output = shell_exec(
+        strtr(':python :script -d :domain -s ":phrase" -n :number', [
+            ':python' => $python,
+            ':script' => $script,
+            ':domain' => $domain,
+            ':phrase' => $phrase,
+            ':number' => $number,
+        ]),
+    );
+} else {
+    $output = shell_exec(
+        strtr(':python :script -a', [
+            ':python' => $python,
+            ':script' => $script,
+        ]),
+    );
+}
 
 // Přesměrovat zpátky
 header('Location: /?message=' . urlencode($output));
